0a9011ef6445c0f38dd6424de72f4828de6c1cb6
zero693 <110007865+zero693@users.noreply.github.com>
2022-07-26 11:17:28 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36";
+
+$cookie = "PHPSESSID=5jk33oj3spp1khm583pl2co01r; session_cookie=5jk33oj3spp1khm583pl2co01r_6084503; session_random=6084503; session_ip=36.84.36.71; session_cntr=ID; ads1350474=1; lang=id; _ga=GA1.2.2071043029.1654482387; _gid=GA1.2.999865337.1654482387; _gat_gtag_UA_48165808_5=1; ads_mellowads=1; tronwallet=TSedrfgL5UP49hpSsz8CgzE9oXRUuhEbVj; googtrans=/en/id; googtrans=/en/id; _data_html=135-1_207-1";
